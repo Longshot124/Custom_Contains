83792206d9b4556e8ad8fb4c6523dbf5f3f81c47
kamra <kamra@LAPTOP-M49P7DNF>
2022-05-04 02:51:11 +0400

    Dynamic version added

---
+++ Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contains_method_by_me
+{
+   public static class Extensions
+    {
+
+        public static bool CustomContains(this string word,string part)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]==part[0])
+                {
+
+                    for (int j = 0; j < part.Length; j++)
+                    {
+                        if (part[j] !=word[i+j])
+                        {
+                            break;
+                        }
+                        if (j==part.Length-1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Contains_method_by_me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "Bu bir custom contains methodudur";
+            //Console.WriteLine(str.CustomContains("cuso"));
+
+            Console.WriteLine("Enter sentence or word");
+            string str = Console.ReadLine();
+            
+             Console.WriteLine("Enter looking part");
+            string part = Console.ReadLine();
+
+            
+            
+            Console.WriteLine(str.CustomContains(part));
+
+
+           
+        }
+
+        
+    }
+}
